28f720e6a58905d30b9a56f210c6ab3dfccaf0ca
Ellena, Ezequiel <eellena@bcr.com.ar>
2021-05-02 12:53:25 -0300

    teelegram bot rasa

--- Telegram/RasaTelegram.py
+++ Telegram/RasaTelegram.py
@@ -9,24 +9,15 @@ app = Flask(__name__)
 @app.route('/Comentarios_bot',methods=["POST"])
 def postComentarios_bot():
 
-    data_comentarios_bot = request.json["message"]["chat"]["id"]
     TOKEN = '1722558802:AAEIR7zKm3Hku0kgBHHpnE5wVwSB8StBLnU'  # Ponemos nuestro TOKEN generado con el @BotFather
     mi_bot = telebot.TeleBot(TOKEN)  # Creamos nuestra instancia "mi_bot" a partir de ese TOKEN
-
     chat_id =  request.json["message"]["chat"]["id"]
-
-    #token = requests.get(url="http://167.86.120.98:7676/tokenrasa").text
-    print(chat_id )
     text = request.json["message"]["text"]
     respuesta = requests.post(url="http://167.86.120.98:5006/webhooks/rest/webhook",
                                   data='{ "sender":"'+ chat_id +'", "message":"' + text + '"}')
-    dictionary_items = respuesta.items()
-    for item in dictionary_items:
-        print(item)
     try:
         txt = respuesta.json()[0]["text"]
     except:
-        # mi_bot.send_message(chat_id, "Muchas Gracias, Para volver a realizar la encuesta envie la palabra 'Hola' ")
         mi_bot.send_message(chat_id,
                             "envie /restart , luego del msjs Reiniciando envie la palabra Hola para empezar nuevamente la encuesta")
     mi_bot.send_message(chat_id, txt)
