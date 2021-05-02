dfdeab55f8eac1c363d7c8262f5a9b1ab7d76975
Ellena, Ezequiel <eellena@bcr.com.ar>
2021-05-02 15:04:02 -0300

    teelegram bot rasa

--- Telegram/RasaTelegram.py
+++ Telegram/RasaTelegram.py
@@ -8,19 +8,23 @@ app = Flask(__name__)
 
 @app.route('/Comentarios_bot',methods=["POST"])
 def postComentarios_bot():
-
-    TOKEN = '1722558802:AAEIR7zKm3Hku0kgBHHpnE5wVwSB8StBLnU'  # Ponemos nuestro TOKEN generado con el @BotFather
-    mi_bot = telebot.TeleBot(TOKEN)  # Creamos nuestra instancia "mi_bot" a partir de ese TOKEN
-    chat_id =  request.json["message"]["chat"]["id"]
-    text = request.json["message"]["text"]
-    respuesta = requests.post(url="http://167.86.120.98:5006/webhooks/rest/webhook",
-                                  data='{ "sender":"'+ chat_id +'", "message":"' + text + '"}')
     try:
-        txt = respuesta.json()[0]["text"]
-    except:
-        mi_bot.send_message(chat_id,
-                            "envie /restart , luego del msjs Reiniciando envie la palabra Hola para empezar nuevamente la encuesta")
-    mi_bot.send_message(chat_id, txt)
+        TOKEN = '1722558802:AAEIR7zKm3Hku0kgBHHpnE5wVwSB8StBLnU'  # Ponemos nuestro TOKEN generado con el @BotFather
+        mi_bot = telebot.TeleBot(TOKEN)  # Creamos nuestra instancia "mi_bot" a partir de ese TOKEN
+        chat_id =  request.json["message"]["chat"]["id"]
+        text = request.json["message"]["text"]
+        respuesta = requests.post(url="http://167.86.120.98:5006/webhooks/rest/webhook",
+                                      data='{ "sender":"'+ chat_id +'", "message":"' + text + '"}')
+        try:
+            txt = respuesta.json()[0]["text"]
+        except:
+            mi_bot.send_message(chat_id,
+                                "envie /restart , luego del msjs Reiniciando envie la palabra Hola para empezar nuevamente la encuesta")
+        mi_bot.send_message(chat_id, txt)
+    except Exception as e:
+        print(e)
+        return "error"
+    return "OK"
 def main():
 	app.run(host='0.0.0.0', debug=True, port="8080")
 
