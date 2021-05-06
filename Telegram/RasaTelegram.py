1e36572f6b24c14870b300bb4ca7fe3e1c8fbd83
Ellena, Ezequiel <eellena@bcr.com.ar>
2021-05-05 21:43:29 -0300

    teelegram bot rasa

--- Telegram/RasaTelegram.py
+++ Telegram/RasaTelegram.py
@@ -1,3 +1,4 @@
+import re
 from urllib import request
 
 import telebot
@@ -5,7 +6,10 @@ import requests
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
 @app.route('/Comentarios_bot',methods=["POST"])
 def postComentarios_bot():
     try:
@@ -16,18 +20,21 @@ def postComentarios_bot():
         text = request.json["message"]["text"]
         print(request.json["message"]["text"])
         print(request.json["message"])
+        """
         if text == "/start":
-            text = 'alebotsalud'
-            resp = requests.post(url="http://167.86.120.98:5006/webhooks/rest/webhook",
-                                      data='{ "sender":"'+ str(chat_id) +'", "message":"' + text + '"}')
+            r = requests.post(url="http://167.86.120.98:5007/webhooks/rest/webhook",
+                              data='{ "sender":"' + str(chat_id) + '", "message":"/restart"}')
+            resp = requests.post(url="http://167.86.120.98:5007/webhooks/rest/webhook",
+                                      data='{ "sender":"'+ str(chat_id) +'", "message":"alebotsalud"}')
             try:
                 txt2 = resp.json()[0]["text"]
                 mytext2 = "\n".join(txt2.split("<br>"))
             except Exception as e:
                 print(e)
             mi_bot.send_message(chat_id, mytext2)
-        else:
-            respuesta = requests.post(url="http://167.86.120.98:5006/webhooks/rest/webhook",
+        """
+        if text is 'A' or text is 'B' or text is 'C' or text is 'D' or text is 'E' or text is 'F' or text is 'G' or text is 'H' or text is 'F':
+            respuesta = requests.post(url="http://167.86.120.98:5007/webhooks/rest/webhook",
                                           data='{ "sender":"' + str(chat_id) + '", "message":"' + text + '"}')
             try:
                 txt = respuesta.json()[0]["text"]
@@ -37,10 +44,11 @@ def postComentarios_bot():
                 print("------------")
                 print("------------")
                 mytext = "\n".join(txt.split("<br>"))
+                mytext = cleanhtml(mytext)
             except Exception as e:
                 print(e)
             text = "/restart"
-            respuesta2 = requests.post(url="http://167.86.120.98:5006/webhooks/rest/webhook",
+            respuesta2 = requests.post(url="http://167.86.120.98:5007/webhooks/rest/webhook",
                                       data='{ "sender":"' + str(chat_id) + '", "message":"' + text + '"}')
 
             try:
@@ -54,7 +62,7 @@ def postComentarios_bot():
                 print(e)
 
             text = 'alebotsalud'
-            respuesta3 = requests.post(url="http://167.86.120.98:5006/webhooks/rest/webhook",
+            respuesta3 = requests.post(url="http://167.86.120.98:5007/webhooks/rest/webhook",
                                        data='{ "sender":"' + str(chat_id) + '", "message":"' + text + '"}')
             try:
                 txt3 = respuesta3.json()[0]["text"]
@@ -63,12 +71,30 @@ def postComentarios_bot():
                 print(txt3)
                 print("------------")
                 print("------------")
-                mytext3 = "\n".join(txt.split("<br>"))
+                mytext3 = "\n".join(txt3.split("<br>"))
             except Exception as e:
                 print(e)
 
             textofinal = mytext + '\n' + mytext3
             mi_bot.send_message(chat_id, textofinal)
+
+        else:
+            r = requests.post(url="http://167.86.120.98:5007/webhooks/rest/webhook",
+                                       data='{ "sender":"' + str(chat_id) + '", "message":"/restart"}')
+            respuesta4 = requests.post(url="http://167.86.120.98:5007/webhooks/rest/webhook",
+                                       data='{ "sender":"' + str(chat_id) + '", "message":"alebotsalud"}')
+            try:
+                txt4 = respuesta4.json()[0]["text"]
+                print("------------")
+                print("------------")
+                print(txt4)
+                print("------------")
+                print("------------")
+                mytext4 = "\n".join(txt4.split("<br>"))
+            except Exception as e:
+                print(e)
+
+            mi_bot.send_message(chat_id, mytext4)
     except Exception as e:
         print(e)
         return "error"
